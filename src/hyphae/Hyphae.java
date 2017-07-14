ee5f46bc290ba700938f5a3388054c1fa99e00da
Brinklesby <brinklesby@gmail.com>
2017-07-14 02:37:50 -0400

    This is the main class

---
+++ src/hyphae/Hyphae.java
@@ -0,0 +1,45 @@
+/*
+ * 
+ * Hyphae is a 2D cellular automaton laboratory and simulation environment.
+ * Using a simple syntax, set up an arbitrarily long list of rules, each one holding an easy to interpret expression.
+ * 
+ * FIN(x):  ORDER:       TODO:
+ *          4            1. Graphical user interface and viewport with multiple sets of controls.
+ *          5                2. Within this environment, allow for user customization of window size, world size, and other things.
+ *          6                    3. Custom tile appearance support (texture packs), each tile represented by a PNG image with the same name as the value of the tile
+ * 
+ *          7            1. Saving / Loading of rules, rulesets, world data, and any combination of all of these three.
+ *          15               2. Export to a variety of other formats, including images, Tiled map data, raw text, masked text, etc...
+ * 
+ *          1            1. Syntax including parentheticals, accurate order of operations, and more complicated math concepts (metamath conditionals, nth dimensional addition, kernels)
+ * 
+ *          2            1. User input responsivity
+ *          14               2. rules activated by user input
+ *          8                2. user 'palette' and cell drawing capabilities
+ * 
+ *          11           1. Support for drawn regions identified by IDs
+ *          12               2. create syntax for cells affecting drawn regions
+ *          13               2. create syntax for specifying drawn region in syntax conditionals, EXAMPLE: if neighborhood center is contained by region 2, then...
+ * 
+ *          3            1. Layered system with spacial union logic, descending order
+ *          16                2. During export, specify layers for export, including display layer
+ * 
+ *          9            1. Simulation settings including speed, starting conditions
+ *          10               2. starting conditions as expression
+ * 
+ */
+
+
+package hyphae;
+
+
+
+public class Hyphae{
+
+    
+    
+    public static void main(String[] args){
+        // TODO code application logic here
+    }
+    
+}
